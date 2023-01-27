24d559adca30d082c15b31867f215ea7caf1e80a
matthias-bs <83612361+matthias-bs@users.noreply.github.com>
2023-01-27 17:23:12 +0000

    Deploying to gh-pages from @ matthias-bs/ESP32-e-Paper-Weather-Display@e57d8cccd325c15edc5f12386744483ef9b21626 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "BresserWeatherSensorReceiver", "index.html", [
+  [ "ESP32-e-Paper-Weather-Display", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ]
