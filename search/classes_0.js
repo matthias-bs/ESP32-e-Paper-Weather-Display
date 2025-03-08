444bc49f4052602e3c208c46abb610dc583be338
matthias-bs <83612361+matthias-bs@users.noreply.github.com>
2025-03-08 21:52:53 +0000

    Deploying to gh-pages from @ matthias-bs/ESP32-e-Paper-Weather-Display@c687073fe51517b7a9024fc89c53eaf6bec1e775 🚀

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['forecast_5frecord_5ftype_1',['Forecast_record_type',['../struct_forecast__record__type.html',1,'']]]
+  ['forecast_5frecord_5ftype_0',['Forecast_record_type',['../struct_forecast__record__type.html',1,'']]]
 ];
